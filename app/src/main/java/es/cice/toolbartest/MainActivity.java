751c3a8f1a865dd27e96a07390fc0c855a85c74e
Joel <joelfernandezreviejo@gmail.com>
2017-01-24 13:08:26 +0100

    Toolbar con vehículos

--- app/src/main/java/es/cice/toolbartest/MainActivity.java
+++ app/src/main/java/es/cice/toolbartest/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -15,8 +17,15 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import es.cice.toolbartest.adapters.CarAdapter;
+import es.cice.toolbartest.model.Car;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar aBar;
     public static final String TAG= "MainActivity";
     private EditText searchET;
     @Override
@@ -25,15 +34,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar=(Toolbar) findViewById(R.id.includedToolbar);
         setSupportActionBar(toolbar);
+        aBar =getSupportActionBar();
+        RecyclerView carRV=(RecyclerView) findViewById(R.id.carRV);
+        carRV.setAdapter(new CarAdapter(this, getData()));
+        carRV.setLayoutManager(new LinearLayoutManager(this));
+
     }
-
-
+    private List<Car> getData()
+    {
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("bla bla bla","Peugeot", R.drawable.vehiculo1, R.drawable.vehiculo1_thumb, "307"));
+        list.add(new Car("bla bla bla","Renault", R.drawable.vehiculo2, R.drawable.vehiculo2_thumb, "357"));
+        list.add(new Car("bla bla bla","Peugeot", R.drawable.vehiculo3, R.drawable.vehiculo3_thumb, "107"));
+        list.add(new Car("bla bla bla","BMW", R.drawable.vehiculo4, R.drawable.vehiculo4_thumb, "207"));
+        list.add(new Car("bla bla bla","Peugeot", R.drawable.vehiculo5, R.drawable.vehiculo5_thumb, "407"));
+        return list;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //creamos el menú
         Log.d(TAG, "onCreateOpcionMenu");
-        MenuInflater inflater = new MenuInflater(this);
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
@@ -41,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        ActionBar aBar =getSupportActionBar();
+
         switch (item.getItemId()){
             case R.id.searchIT:
                 Log.d(TAG, "Search item...");
@@ -63,24 +85,33 @@ public class MainActivity extends AppCompatActivity {
                             String searchText=searchET.getText().toString();
                             //empezar la busqueda y hacer desaparecer el teclado
                             Log.d(TAG, "search text ...." + searchText);
-                            //Hacer desaparecer el teclado
+                            //Hacer desaparecer el teclado, llamamos a un servicio del sistema q se encarga de hacer aparecer/desaparecer el teclado
                             InputMethodManager imn=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                             imn.hideSoftInputFromWindow(searchET.getWindowToken(),0);
+                            //volvemos a poner el menú
+                            aBar.setDisplayShowCustomEnabled(false);
+                            //para q desaparezca el título
+                            aBar.setDisplayShowTitleEnabled(true);
+                            //empezar la busqueda. Mostramos lista de vehículos
+
                             return true;
                         }
                         return false;
 
                     }
                 });
-
+                //mostamos el teclado
+                InputMethodManager imn=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imn.showSoftInput(searchET, InputMethodManager.SHOW_IMPLICIT);
+                searchET.requestFocus();
                 break;
             case R.id.settingIT:
                 Log.d(TAG, "Setting item...");
                 break;
             case R.id.aboutIT:
                 Log.d(TAG, "About...");
-                TextView txt = (TextView)findViewById(R.id.txtDesc);
-                txt.setText("Esta aplicación muestra una barra de herramientas.");
+                //TextView txt = (TextView)findViewById(R.id.txtDesc);
+                //txt.setText("Esta aplicación muestra una barra de herramientas.");
                 break;
         }
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/es/cice/toolbartest/adapters/CarAdapter.java
@@ -0,0 +1,144 @@
+package es.cice.toolbartest.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import es.cice.toolbartest.CarDetailActivity;
+import es.cice.toolbartest.R;
+import es.cice.toolbartest.model.Car;
+
+/**
+ * Created by cice on 20/1/17.
+ */
+
+public class CarAdapter extends RecyclerView.Adapter<CarAdapter.ViewHolder> {
+
+    private List<Car> carList;
+    private Context ctx;
+    public CarAdapter(Context ctx, List<Car> list){
+        carList=list;
+        this.ctx=ctx;
+    }
+    /*
+        Este método se llama cada vez que sea necesario construir una nueva fila
+
+      */
+
+    /**
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     * <p>
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     * <p>
+     * The new ViewHolder will be used to display items of the adapter using
+     * {@link #(ViewHolder, int, List)}. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+     *
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see #getItemViewType(int)
+     * @see #onBindViewHolder(ViewHolder, int)
+     */
+    @Override
+    //para crear un fila nueva
+    public CarAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Obtenemos el inflater necesario para construir una fila definida en xml
+    //el inflater asociado a ese contexto
+        LayoutInflater inflater=LayoutInflater.from(ctx);
+        //construimos la fila
+        View row = inflater.inflate(R.layout.car_row, parent, false);
+        ViewHolder holder= new ViewHolder(row);
+        return holder;
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+     * position.
+     * <p>
+     * Note that unlike {@link ListView}, RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the <code>position</code> parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
+     * have the updated adapter position.
+     * <p>
+     * Override {@link(ViewHolder, int, List)} instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(CarAdapter.ViewHolder holder, int position) {
+        //se recicla la fila
+        holder.carIV.setImageResource(carList.get(position).getMiniatura());
+        holder.carTV.setText(carList.get(position).getFabricante() + " " +
+        carList.get(position).getModelo());
+
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return carList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        //además de guardar la fila, para minimizar el tiempo para reciclar una fila mantiene las referencias de los widgets que hay q modificar
+        //el imageview y el textview
+        private ImageView carIV;
+        private TextView carTV;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            carIV = (ImageView)itemView.findViewById(R.id.carThumbIV);
+            carTV = (TextView)itemView.findViewById(R.id.carNameTV);
+            carIV.setOnClickListener(new View.OnClickListener() {
+                /**
+                 * Called when a view has been clicked.
+                 *
+                 * @param v The view that was clicked.
+                 */
+                @Override
+                public void onClick(View v) {
+                    Log.d("CardViewHolder", "old position: "+ getOldPosition());
+                    Log.d("CardViewHolder", "layout position: " + getLayoutPosition());
+                    Log.d("CardViewHolder", "adapter position: " + getAdapterPosition());
+                    Intent intent=new Intent(ctx, CarDetailActivity.class);
+                    intent.putExtra(CarDetailActivity.IMAGEN_EXTRA,
+                            carList.get(getAdapterPosition()).getImagen());
+                    intent.putExtra(CarDetailActivity.DESCRIPCION_EXTRA,
+                            carList.get(getAdapterPosition()).getDescripcion());
+                    intent.putExtra(CarDetailActivity.FABRICANTE_EXTRA,
+                            carList.get(getAdapterPosition()).getFabricante());
+                    intent.putExtra(CarDetailActivity.MODELO_EXTRA,
+                            carList.get(getAdapterPosition()).getModelo());
+                    ctx.startActivity(intent);
+
+                }
+            });
+        }
+    }
+}
